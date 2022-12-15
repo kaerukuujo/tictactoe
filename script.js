afedc806371e64ed4aed8f3e095b16788b824a39
kaerukuujo <mrjolyne@gmail.com>
2022-12-15 18:21:48 +0000

    basic html layout + logic in JS file added

--- script.js
+++ script.js
@@ -1,17 +1,57 @@
+
+
 const gameBoard = (function(){
     let gameBoard = [['', '', ''], ['', '', ''], ['', '', '']];
 
-    const setGameBoard = (() => {
+    const resetGameBoard = (() => {
         gameBoard = [['', '', ''], ['', '', ''], ['', '', '']];
+        return gameBoard;
     })();
 
-    return { gameBoard, setGameBoard, }
+    return { gameBoard, resetGameBoard}
 })();
 
 const displayController = (function(){
+
+    function setState() {
+        let gameSpace = document.querySelectorAll('.gameState');
+        let preGame = document.querySelector('.preGame');
+    let playSpace = document.querySelector('.playSpace');
+    let postGame = document.querySelector('.postGame');
+
+    const preGameState = (() => {
+        for(let i = 0; i < gameSpace.length; i++){
+            gameSpace[i].setAttribute("data-state", "inactive");
+        }
+        preGame.setAttribute("data-state", "active");
+        console.log("pregame State");
+    })();
+
+    const playGameState = (() => {
+        for(let i = 0; i < gameSpace.length; i++){
+            gameSpace[i].setAttribute("data-state", "inactive");
+        }
+        playSpace.setAttribute("data-state", "active");
+        console.log("playgame State");
+    })();
+
+    const postGameState = (() => {
+        for(let i = 0; i < gameSpace.length; i++){
+            gameSpace[i].setAttribute("data-state", "inactive");
+        }
+        postGame.setAttribute("data-state", "active");
+        console.log("postgame State");
+    })();
+    }
+
     
-    return { preGameState, playGameState, postGameState }
 })();
+
+const createPlayer = (playerName, icon) => {
+    const playerIcon = icon;
+    return { playerName, playerIcon };
+};
+
 
 
 
